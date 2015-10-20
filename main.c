afa16e105c12363c4c7965241ea2ddd23f88d1b8
mAzurkovic <mattias@zurkovic.com>
2015-10-19 19:22:04 -0700

    inint

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//*******************************||
+// Scientific Notation Converter ||
+// Author: Mattias A. Zurkovic   ||
+//*******************************||     
+
+int main(void)
+{
+
+    printf("Test main.c");   
+
+}
